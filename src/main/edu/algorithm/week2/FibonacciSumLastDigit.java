0f6a48662d925ab54d1a606dc3cc7eb81ba5156a
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-04 16:54:29 +0200

    FibonacciSumLastDigit - fast method

---
+++ src/main/edu/algorithm/week2/FibonacciSumLastDigit.java
@@ -0,0 +1,37 @@
+package main.edu.algorithm.week2;
+
+import java.util.*;
+
+public class FibonacciSumLastDigit {
+    private static long getFibonacciSumNaive(long n) {
+        if (n <= 1)
+            return n;
+
+        long previous = 0;
+        long current = 1;
+        long sum = 1;
+
+        for (long i = 0; i < n - 1; ++i) {
+            long tmp_previous = previous;
+            previous = current;
+            current = tmp_previous + current;
+            sum += current;
+        }
+
+        return sum % 10;
+    }
+
+    private static long getFibonacciSumFast(long n) {
+        long a = FibonacciHuge.getFibonacciHugeFast(n+2,10);
+        if (a==0)
+            return (a+10-1)%10;
+        return (a-1)%10;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long s = getFibonacciSumFast(n);
+        System.out.println(s);
+    }
+}
+
