5f866f6c4d7d5a2a298c81b79a93014e0f9bdf83
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-04 18:07:48 +0200

    Sum of squares of Fibonacci numbers

---
+++ src/main/edu/algorithm/week2/FibonacciSumSquares.java
@@ -0,0 +1,37 @@
+package main.edu.algorithm.week2;
+
+import java.util.*;
+
+public class FibonacciSumSquares {
+    private static long getFibonacciSumSquaresNaive(long n) {
+        if (n <= 1)
+            return n;
+
+        long previous = 0;
+        long current = 1;
+        long sum = 1;
+
+        for (long i = 0; i < n - 1; ++i) {
+            long tmp_previous = previous;
+            previous = current;
+            current = tmp_previous + current;
+            sum += current * current;
+        }
+
+        return sum % 10;
+    }
+
+    private static long getFibonacciSumSquaresFast(long n) {
+        long a = FibonacciPartialSum.getFibonacciPartialSumFast(n-1, n);
+        long b = FibonacciHuge.getFibonacciHugeFast(n, 10);
+        return (a*b)%10;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long s = getFibonacciSumSquaresFast(n);
+        System.out.println(s);
+    }
+}
+
