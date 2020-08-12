8b8ecdc19589be4779e3c0bf131927b0bd482794
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-12 20:43:03 +0200

    maximum prizes for children with distinct pairwise prize counts

---
+++ src/main/edu/algorithm/week3/DifferentSummands.java
@@ -0,0 +1,34 @@
+package main.edu.algorithm.week3;
+
+import java.util.*;
+
+public class DifferentSummands {
+    private static List<Integer> optimalSummands(int n) {
+        List<Integer> summands = new ArrayList<>();
+        int previous = 0;
+
+        while (n>0){
+            previous++;
+            if (n-previous>=0) {
+                summands.add(previous);
+                n-=previous;
+            }
+            else {
+                summands.set(summands.size() - 1, summands.get(summands.size() - 1) + n);
+                n=0;
+            }
+        }
+        return summands;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> summands = optimalSummands(n);
+        System.out.println(summands.size());
+        for (Integer summand : summands) {
+            System.out.print(summand + " ");
+        }
+    }
+}
+
