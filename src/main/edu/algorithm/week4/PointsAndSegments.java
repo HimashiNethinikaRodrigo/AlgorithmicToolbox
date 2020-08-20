62db5c252171b19b5211e44bf1ea16dffdc110ef
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-20 17:14:16 +0200

    create array by inserting all start, end and check points

--- src/main/edu/algorithm/week4/PointsAndSegments.java
+++ src/main/edu/algorithm/week4/PointsAndSegments.java
@@ -1,12 +1,30 @@
 package main.edu.algorithm.week4;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class PointsAndSegments {
 
     private static int[] fastCountSegments(int[] starts, int[] ends, int[] points) {
         int[] cnt = new int[points.length];
-        //write your code here
+        Value[] allPoints = new Value[starts.length*2+points.length];
+        int j= 0;
+
+        for (int i = 0; i < starts.length; i++)
+            allPoints[j++] = new Value(starts[i], 1, i);
+
+        for (int i = 0; i < ends.length; i++)
+            allPoints[j++] = new Value(ends[i], 3, i);
+
+        for (int i = 0; i < points.length; i++)
+            allPoints[j++] = new Value(points[i], 2, i);
+
+
+
+        Arrays.sort(allPoints, PointsAndSegments::compare);
+
+
         return cnt;
     }
 
@@ -22,6 +40,26 @@ public class PointsAndSegments {
         return cnt;
     }
 
+    private static int compare(Value o1, Value o2) {
+        if (o1.point == o2.point)
+            return Integer.compare(o1.type, o2.type);
+        return Integer.compare(o1.point, o2.point);
+    }
+
+    /**
+     *type
+     * 1= start point
+     * 3= end point
+     * 2= check point
+     */
+    private static class Value{
+        int point, type, index;
+        Value(int point, int type, int index){
+            this.point = point;
+            this.type = type;
+            this.index = index;
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n, m;
@@ -38,7 +76,8 @@ public class PointsAndSegments {
             points[i] = scanner.nextInt();
         }
         //use fastCountSegments
-        int[] cnt = naiveCountSegments(starts, ends, points);
+//        int[] cnt = naiveCountSegments(starts, ends, points);
+        int[] cnt = fastCountSegments(starts, ends, points);
         for (int x : cnt) {
             System.out.print(x + " ");
         }
