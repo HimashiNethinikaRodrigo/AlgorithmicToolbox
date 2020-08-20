b69429c65122ba30724decf7a9255ae9135592a7
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-20 13:27:06 +0200

    bug fix for counting Inversions

--- src/main/edu/algorithm/week4/Inversions.java
+++ src/main/edu/algorithm/week4/Inversions.java
@@ -1,56 +1,44 @@
 package main.edu.algorithm.week4;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Inversions {
 
     private static long getNumberOfInversions(int[] a, int[] b, int left, int right) {
         long numberOfInversions = 0;
-        if (right == left ) {
-            return numberOfInversions;
+        if (right > left) {
+            int mid = (right + left) / 2;
+            numberOfInversions += getNumberOfInversions(a, b, left, mid);
+            numberOfInversions += getNumberOfInversions(a, b, mid + 1, right);
+            numberOfInversions += mergeAndCountInversions(a, b, left, mid, right);
         }
-        int mid = left + ((right - left) / 2);
-        numberOfInversions += getNumberOfInversions(a, b, left, mid);
-        numberOfInversions += getNumberOfInversions(a, b, mid+1, right);
-        numberOfInversions += mergeAndCountInversions(a, b, left, mid, right);
         return numberOfInversions;
     }
 
     private static int mergeAndCountInversions(int[] a, int[] b, int left, int mid, int right) {
-        int[] n1 = new int[mid-left];
-        int[] n2 = new int [right-mid];
         int inversions = 0;
-        for (int i = left; i < n1.length ; i++) {
-            n1[i] = a[left+i];
-        }
-        for (int i = 0; i < n2.length; i++) {
-            n2[i] = a[mid+i];
-        }
-        int i=0, j=0, k = left;
 
-        while (i < n1.length && j < n2.length) {
-            if (n1[i] <= n2[j]) {
-                b[k] = n1[i];
-                i++;
+        int i = left, j = mid + 1, k = left;
+
+        while (i <= mid && j <= right) {
+            if (a[i] <= a[j]) {
+                b[k++] = a[i++];
             } else {
-                b[k] = n2[j];
-                inversions++;
-                j++;
-            }
-            k++;
-
-            while (i < n1.length) {
-                b[k] = n1[i];
-                i++;
-                k++;
-            }
-
-            while (j < n2.length) {
-                b[k] = n2[j];
-                j++;
-                k++;
+                b[k++] = a[j++];
+                inversions += mid - i + 1;
             }
         }
+        while (i <= mid)
+            b[k++] = a[i++];
+
+
+        while (j <= right)
+            b[k++] = a[j++];
+
+
+        if (right + 1 - left >= 0) System.arraycopy(b, left, a, left, right + 1 - left);
+
+
         return inversions;
     }
 
@@ -62,7 +50,7 @@ public class Inversions {
             a[i] = scanner.nextInt();
         }
         int[] b = new int[n];
-        System.out.println(getNumberOfInversions(a, b, 0, a.length));
+        System.out.println(getNumberOfInversions(a, b, 0, a.length - 1));
     }
 }
 
