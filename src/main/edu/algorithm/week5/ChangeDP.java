f8a8107e445c600b118f408feeedf3791f4a2075
HimashiNethinikaRodrigo <himashirodrigo23@gmail.com>
2020-08-29 12:53:44 +0200

    send denominations to get change method

--- src/main/edu/algorithm/week5/ChangeDP.java
+++ src/main/edu/algorithm/week5/ChangeDP.java
@@ -3,16 +3,15 @@ package main.edu.algorithm.week5;
 import java.util.Scanner;
 
 public class ChangeDP {
-    private static int getChange(int m) {
-        //write your code here
-        return m / 4;
+    private static int getChange(int money, int[] denominations) {
+        return money / 4;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int m = scanner.nextInt();
-        System.out.println(getChange(m));
-
+        int money = scanner.nextInt();
+        int[] denominations = {1, 3, 4};
+        System.out.println(getChange(money, denominations));
     }
 }
 
